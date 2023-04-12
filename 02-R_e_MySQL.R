371fe340d106a06e59a59e8ad9519d4fb00d7f03
Eduardo Brandao <eduardo.ads1814@gmail.com>
2023-04-12 20:18:50 -0300

    carregando e analisando dados do MySQL - parte 2

--- 02-R_e_MySQL.R
+++ 02-R_e_MySQL.R
@@ -15,6 +15,7 @@ install.packages('dbplyr')    # pacote que contém drive necessário para conect
 library(RMySQL)
 library(ggplot2)
 library(dbplyr)
+library(dplyr)
 
 
 
@@ -23,5 +24,50 @@ library(dbplyr)
 
 con = dbConnect(MySQL(), user = 'root', password = '', dbname = 'titanicDB', host = "localhost")
 
+
+# Query
+
+qry <- "select pclass, survived, avg(age) as media_idade from titanic where survived = 1 group by pclass, survived;"
+
+dbGetQuery(con, qry)
+
+
+# Plot (gráfico de barras)
+
+dados <- dbGetQuery(con, qry)
+
+ggplot(dados, aes(pclass, media_idade)) +
+  geom_bar(stat = "identity")
+
+
+
+
+# Outra forma de fazer a conexão com o MySQL
+
+
+# Conectando no MySQL com dplyr
+
+con2 <- src_mysql(dbname = "titanicdb", user = 'root', password = '', host = 'localhost')
+
+# Coletando e agrupando os dados
+# chama a conexao com 'con2', concatena com o nome da tabela que eu queroa cessar 'tbl'
+# concatena com 'select' para retornar algumas variáveis (colunas) e concatena com o filtro 'filter' e faz o collect para coletar os dados
+# seleciona todas as colunas pclass, sex, age, survived, fare onde o resultado de survived for igual a 0
+
+dados2 <- con2 %>%
+  tbl("titanic") %>%
+  select(pclass, sex, age, survived, fare) %>%
+  filter(survived == 0) %>%
+  collect()
+
+
+# Manipulando os dados
+
+dados3 <- con2 %>%
+  tbl("titanic") %>%
+  select(pclass, sex, survived) %>%
+  group_by(pclass, sex) %>%
+  summarise(survival_ratio = avg(survived)) %>%
+  collect() 
 
 
