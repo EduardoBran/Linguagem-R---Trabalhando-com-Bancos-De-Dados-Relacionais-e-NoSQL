4027041ab644843ee5a0648b92a310f17172e3ec
Eduardo Brandao <eduardo.ads1814@gmail.com>
2023-04-14 17:06:36 -0300

    trabalhando com R e SQLite - parte 3

--- 03-R_e_SQLite.R
+++ 03-R_e_SQLite.R
@@ -47,7 +47,7 @@ dbWriteTable(con, 'mtcars', mtcars, row.names = TRUE)
 
 dbListTables(con)
 dbExistsTable(con, 'mtcars')
-dbExistsTable(con, 'mtcars2')
+dbExistsTable(con, 'mtcars122')
 dbListFields(con, 'mtcars')      #  lista os campos ou colunas da tabela mtcars 
 
 
@@ -56,7 +56,7 @@ dbListFields(con, 'mtcars')      #  lista os campos ou colunas da tabela mtcars
 dbReadTable(con, 'mtcars')
 View(dbReadTable(con, 'mtcars'))
 
-
+View(mtcars)
 
 # Criando apenas a definição da tabela
 
@@ -70,5 +70,126 @@ dbReadTable(con, 'mtcars2')
 View(dbReadTable(con, 'mtcars2'))
 
 
+
+# Executando consultas no banco de dados
+
+# - A consulta seleciona a coluna row_names da tabela mtcars e armazena o resultado na variável rs através da função dbSendQuery.
+#   Em seguida, os dados são buscados e carregados na variável dados utilizando a função fetch, que recebe como parâmetros o resultado
+#   da consulta rs e o número máximo de linhas a serem recuperadas (n = -1 significa que todas as linhas serão recuperadas).
+
+query = "select row_names from mtcars"
+
+rs = dbSendQuery(con, query)
+
+dados = fetch(rs, n = -1)
+
+dados
+class(dados)   # dataframe
+
+
+
+
+# Executando consultas no banco de dados 
+
+# - Aqui usamos o loop while para enquanto não tiver concluído o 'rs' , vai imprimindo os dados linha a linha
+#   Neste exemplo é diferente do exemplo acima, no exemplo de cima a gente pega todo o resultado da query e grava tudo de uma vez
+#   no dataframe e com isso caso a query retorne muitos dados, isso compromete a memória do computador.
+#   A alternativa é usar o loop while e ir gravando linha a linha (será mais lento, mas não compromete tanta memória)
+
+query = "select row_names from mtcars"
+rs = dbSendQuery(con, query)
+
+while (!dbHasCompleted(rs)) {
+  
+  dados = fetch(rs, n = 1)
+  print(dados$row_names)
+}
+
+
+
+
+# Executando consultas no banco de dados
+
+query = 'select disp, hp from mtcars where disp > 160'
+rs = dbSendQuery(con, query)
+dados = fetch(rs, n = -1)
+dados
+
+
+
+
+# Executando consultas no banco de dados
+
+# - A consulta SQL acima seleciona a coluna "row_names" e a média da coluna "hp" da tabela "mtcars", agrupando por "row_names".
+#   O comando "group by" é usado para agrupar os dados por valores únicos na coluna "row_names", de modo que a média de "hp" seja
+#   calculada para cada grupo único de valores na coluna "row_names".
+
+query = 'select row_names, avg(hp) from mtcars group by row_names'
+rs = dbSendQuery(con, query)
+dados = fetch(rs, n = -1)
+dados
+
+
+
+
+# Criando uma tabela a partir de um arquivo externo (iris.csv)
+
+dbWriteTable(con, 'iris', 'iris.csv', sep = ',', header = T)
+
+dbListTables(con)
+
+dbReadTable(con, 'iris')
+
+
+
+# Encerrando a conexão
+
+dbDisconnect(con)
+
+
+
+# Carregando mais dados no banco de dados
+
+
+# Conectando o banco
+
+drv = dbDriver("SQLite")
+
+con = dbConnect(drv, dbname = 'exemplo.db')
+
+
+# Criando uma tabela a partir de um arquivo externo (iris.csv)
+
+dbWriteTable(con, 'indices', 'indice.csv', sep = '|', header =T)
+
+# Excluindo a tabela
+
+dbRemoveTable(con, 'indices')
+dbListTables(con)
+
+
+df <- dbReadTable(con, 'indices')
+View(df)
+
+str(df)
+
+# fazendo um loop para verificar o tipo de cada coluna
+sapply(df, class)
+
+
+# plotando um histograma
+hist(df$setembro)
+
+# verificando a média de cada coluna com mês
+df_mean <- unlist(lapply(df[, c(4, 5, 6, 7, 8)], mean))
+df_mean
+
+View(df_mean)
+
+
+
+# Encerrando a conexão
+
+dbDisconnect(con)
 
 
