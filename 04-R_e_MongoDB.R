d179257ae4152166e3d292a935d6ab85aea6c466
Eduardo Brandao <eduardo.ads1814@gmail.com>
2023-04-17 17:52:44 -0300

    MapReduce para analise de dados com R e MongoDB - parte 3

--- 04-R_e_MongoDB.R
+++ 04-R_e_MongoDB.R
@@ -28,6 +28,7 @@ install.packages('mongolite')
 library(ggplot2)
 library("devtools")
 library(mongolite)
+library(dplyr)
 
 
 # Help
@@ -54,6 +55,97 @@ View(dados)
 # Verifica o número de registros no conjunto de dados
 
 con$count('{}')
+
+
+
+# Busca uma amostra de dados somente com propriedades do tipo House e suas políticas de cancelamento
+
+
+# buscando e exbindo o resultado de todas as linhas cujo a coluna "property_type" for igual a "House"
+
+amostra1 <- con$find(
+  query = '{ "property_type" : "House" }'
+)
+
+# buscando todas as linhas cujo a coluna "property_type" for igual a "House"
+# especificando que apenas as colunas "property_type" e "cancellation_policy" devem ser exibidas no resultado.
+
+amostra1_fields <- con$find(
+  query = '{ "property_type" : "House" }',
+  fields = '{ "property_type" : true, "cancellation_policy" : true }'
+)
+
+# não exibindo o campo ID
+
+amostra2 <- con$find(
+  query = '{ "property_type" : "House" }',
+  fields = '{ "property_type" : true, "cancellation_policy" : true, "_id": false }'
+)
+
+
+# Ordenando o resultado (1 = crescente | -1 = decrescente)
+
+amostra3 <- con$find(
+  query = '{ "property_type" : "House" }',
+  fields = '{ "property_type" : true, "cancellation_policy" : true, "_id": false }',
+  sort = '{ "cancellation_policy" : -1 }' 
+)
+
+
+
+# Vamos agregar os dados e retornar a média de reviews (total de avaliações por tipo de propriedade)
+
+
+# - O campo "_id" é um campo especial utilizado pelo MongoDB para representar o valor pelo qual os documentos são agrupados na operação
+#   de agregação
+
+amostra4 <- con$aggregate(
+  '[{ "$group":{"_id": "$property_type", "count": {"$sum":1}, "average": {"$avg":"$number_of_reviews"}} }]',
+  options = '{ "allowDiskUse" : true }',
+)
+
+names(amostra4) <- c("tipo_propriedade", "contagem", "media_reviews")
+
+View(amostra4)
+
+
+# Utilizando R
+
+# - group_by(property_type): Esta é uma função do dplyr que agrupa os dados pelo campo property_type, ou seja, os dados são divididos
+#   em grupos com base nos diferentes valores do campo property_type.
+# - summarize(contagem = n(), media_reviews = mean(number_of_reviews)): Esta é outra função do dplyr que realiza o resumo dos dados
+#   agrupados. A função n() retorna o número de observações em cada grupo, ou seja, o total de registros para cada tipo de propriedade,
+#   que é atribuído à nova coluna contagem. A função mean(number_of_reviews) calcula a média dos valores do campo number_of_reviews
+#   em cada grupo, ou seja, a média dos reviews para cada tipo de propriedade, que é atribuída à nova coluna media_reviews.
+
+amostra4_r <- dados %>%
+  group_by(property_type) %>%
+  summarize(contagem = n(), media_reviews = mean(number_of_reviews))
+
+names(amostra4_r) <- c("tipo_propriedade", "contagem", "media_reviews")
+
+View(amostra4_r)
+
+
+
+# Gerando o resultado em um gráfico de barras
+
+ggplot(aes(tipo_propriedade, contagem), data = amostra4) + geom_col()
+ggplot(aes(tipo_propriedade, contagem), data = amostra4_r) + geom_col()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
